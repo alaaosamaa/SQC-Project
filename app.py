915f32eceb433806fbab2fff9aef2befa18069ac
alaaosamaa <youremail@domain.com>
2021-10-14 21:42:59 +0200

    Add QC and Add QC results

--- app.py
+++
@@ -1,9 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-app = dash.Dash(__name__, suppress_callback_exceptions=True, meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0'}],
-                            external_stylesheets=[dbc.themes.MINTY, dbc.themes.BOOTSTRAP]
-                            
-                            )
-server = app.server
